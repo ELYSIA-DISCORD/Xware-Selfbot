34750838a776de8d21e8f194842112a729e28c7b
DaGamingMan2018 <58614587+DaGamingMan2018@users.noreply.github.com>
2020-11-08 19:04:33 +1300

    added files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Jai-Automated-User-Discord
 Discord Self Bot / Automated User!
+
+# Installation
+
+Run the `install.bat` file so it can install all the files\
+Wait until it's Done
+# Setup
+In order to get the automated user / Self bot to work.\
+1. Navigate to the botconfig.json file.
+2. Change the token to the users token. (Watch a tutorial if you don't know how) (Just type in how to get my token on yt)
+3. Change prefix to a desired prefix
+4. Change botid to the users ID
+5. Nitro sniper is optionable. So change it to true if you want to snipe nitro gifts in servers you're in
+6. Whitelisted is also Optionable but make sure you have your ID in it.
+7. And you're done!
+Your config file should also look like this ```js{
+    "token": "token-here",
+    "prefix": "prefix-here",
+    "botID": "bot-id",
+    "nitro_sniper": false,
+    "whitelisted": [""]
+}
+```
+
+
+
+# Activating the self bot / automated user
+Just simply run the `start.bat` file!
+
+# Jai Automated User Discord RULES
+1. Don't just claim the files as yours. That's just dumb...
+2. That's all
+
+# User automation / Self bot risks
+I would strictly recommend that you don't use your main acc's token for this\
+You have the risk of your Account get `DISABLED` or even `BANNED!`\
+So use on an alternative account.
+I am also not responsible for any actions taken when using this project.
+Be Careful and have Fun!
---
+++ index.js
@@ -0,0 +1,245 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const {token, prefix, botID, whitelisted, nitro_sniper} = require("./botconfig.json");
+const colors = require("colors");
+const axios = require("axios");
+const moment = require("moment");
+
+
+console.clear()
+console.log(colors.red("WARNING: Use at your own risk!"))
+console.log(colors.rainbow("Logging in..."))
+bot.on("ready", async() => {
+    console.clear()
+    bot.login(token)
+    console.log(colors.rainbow(`
+    ░░░░░██╗░█████╗░██╗░░░██╗  ░██████╗███████╗██╗░░░░░███████╗██████╗░░█████╗░████████╗
+    ░░░░░██║██╔══██╗╚██╗░██╔╝  ██╔════╝██╔════╝██║░░░░░██╔════╝██╔══██╗██╔══██╗╚══██╔══╝
+    ░░░░░██║███████║░╚████╔╝░  ╚█████╗░█████╗░░██║░░░░░█████╗░░██████╦╝██║░░██║░░░██║░░░
+    ██╗░░██║██╔══██║░░╚██╔╝░░  ░╚═══██╗██╔══╝░░██║░░░░░██╔══╝░░██╔══██╗██║░░██║░░░██║░░░
+    ╚█████╔╝██║░░██║░░░██║░░░  ██████╔╝███████╗███████╗██║░░░░░██████╦╝╚█████╔╝░░░██║░░░
+    ░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░  ╚═════╝░╚══════╝╚══════╝╚═╝░░░░░╚═════╝░░╚════╝░░░░╚═╝░░░
+    `))
+    console.log(colors.green(`${bot.user.tag} has logged in with an ID of ${botID}`))
+    console.log(colors.green(`Self bot by ${bot.users.get("415387271240613898").tag} with an ID of 415387271240613898`))
+    console.log(colors.green("To view bot commands use the command: cmds (Or if you renamed the command use that cmd)"))
+    bot.user.setActivity("with Discord API", {type: "PLAYING"})
+})
+
+
+bot.on("message", message => {
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0]
+    let args = messageArray.slice(1)
+    if(!message.content.startsWith(prefix)) return;
+    if(message.author.id === botID) return;
+    if(whitelisted.includes(message.author.id)){
+        if(cmd === prefix + "hi") {
+            message.reply("Hello")
+        }
+        if(cmd === prefix + "dmall") {
+            if(!args.join(" ")) return message.reply("Please put in a message")
+            let users = message.guild.members.forEach(u => {
+                if(u.user.bot) return console.log(colors.rainbow(`Couldn't send a message to ${u.user.tag}`))
+                if(u.user.id === botID) return;
+                u.user.send(args.join(" "))
+                console.log(`Sent a message to ${u.user.tag} with the following message: ${args.join(" ")}`)
+            })
+        }
+        if(cmd === prefix + "nitro") {
+            let embed = new Discord.RichEmbed()
+            let user = message.guild.members.get(args[0])
+            if(!user) return message.reply("Please put a users ID")
+                embed.setTitle("🎉 Congratulations!")
+                embed.setColor("RANDOM")
+                embed.setDescription("You have been lucky enough to get **Free** Discord **Nitro**! To get it simply press this **[Link](https://www.youtube.com/watch?v=dQw4w9WgXcQ)**")
+                embed.setImage("https://cdn.discordapp.com/attachments/739274510544404480/757460356384948254/BlaringPointedInvisiblerail-size_restricted.gif")
+                embed.setFooter(`${bot.user.username}`)
+                user.send(embed)
+                console.log(`Successfully dm'ed ${user}`)
+        }
+        if(cmd === prefix + "createchannels") {
+            setInterval(() => {
+                message.guild.createChannel("RAIDED BY JAY", {
+                type: "text"
+                })
+                })
+        }
+        if(cmd === prefix + "createroles") {
+            setInterval(() => {
+                message.guild.createRole({
+                    name:" RAIDED BY JAY",
+                    color: "RANDOM"
+                })
+                })
+        }
+        if(cmd === prefix + "cmds") {
+            let embed = new Discord.RichEmbed()
+            embed.setTitle("Self bot commands")
+            embed.setColor("BLUE")
+            embed.addField("nitro <userID>", "Sends the user a fake Nitro Link")
+            embed.addField("dmall <args>", "Dm every single member (Filters bots) whatever you say")
+            embed.addField("createchannels", "Creates a whole ton of channels")
+            embed.addField("createroles", "Creates a whole ton of roles")
+            embed.addField("ping", "Shows the message edit speed and the websocket latency")
+            embed.addField("ghostping", "Ghost pings a user")
+            embed.addField("spam", "Spam an amount of messages you desire")
+            embed.addField("avatar", "Get a mentioned users avatar")
+            embed.addField("embedmsg", "Creates an embed with your text in it")
+            embed.addField("nitromsg", "Shows a free nitro gift message ;)")
+            embed.addField("sendall", "Sends every channel a text you say")
+            embed.addField("advanceping", "Ghost ping a user but it will say a test you said")
+            embed.addField("pingall", "Ghost pings whole server with a text you said")
+            embed.setFooter("Self bot commands")
+            message.channel.send(embed)
+        }
+        if(cmd === prefix + "ping") {
+            message.channel.send("Pinging...").then(msg => {
+                let embed = new Discord.RichEmbed()
+                embed.setTitle("Pong!")
+                embed.addField("MessageEdit", `${msg.createdTimestamp - message.createdTimestamp}ms`)
+                embed.addField("Websocket Latency", `${bot.ping}ms`)
+                msg.edit(embed)
+                msg.edit("")
+                })
+        }
+        if(cmd === prefix + "ghostping") {
+            let userID = args[0]
+            if(!userID) return message.reply("Please put a users ID")
+            message.channel.send(`<@!${userID}>`).then(m => {
+                m.delete()
+            })
+            if(message.deletable) {
+                message.delete()
+            } else {
+                return console.log(colors.red("Could not delete message!"))
+            }
+        }
+        if(cmd === prefix + "spam") {
+            if (!args[0] || !/\d{1,2}/ig.test(args[0])) {
+              return message.channel.send("Please specify the number of messages to spam.")
+            } else {
+              var spamAmount = args[0]
+            }
+            if (!args[1]) {
+              return message.channel.send("I can't spam nothing.")
+            } else {
+              args.splice(0, 1)
+              var spamContent = args.join(" ")
+            }
+            for (var i = 0; i < spamAmount; i++) {
+              message.channel.send(spamContent)
+            }
+        }
+        if(cmd === prefix + "avatar") {
+            let user = message.mentions.users.first();
+            if(user){
+                message.channel.send(user.displayAvatarURL)
+            } else {
+                message.reply("What the fuck. what user u want me to do")
+            }
+        }
+        if(cmd === prefix + "embedmsg") {
+            let embed = new Discord.RichEmbed()
+            embed.setDescription(`${args.join(" ")}`)
+            embed.setThumbnail(`${bot.user.displayAvatarURL}`)
+            embed.setColor("RANDOM")
+            embed.setFooter(`${bot.user.tag}`)
+            message.channel.send(embed)
+        }
+        if(cmd === prefix + "nitromsg") {
+            let embed = new Discord.RichEmbed()
+                embed.setTitle("🎉 Congratulations!")
+                embed.setColor("RANDOM")
+                embed.setDescription("You have been lucky enough to get **Free** Discord **Nitro**! To get it simply press this **[Link](https://www.youtube.com/watch?v=dQw4w9WgXcQ)**")
+                embed.setImage("https://cdn.discordapp.com/attachments/739274510544404480/757460356384948254/BlaringPointedInvisiblerail-size_restricted.gif")
+                embed.setFooter(`${bot.user.username}`)
+                message.channel.send(embed)
+        }
+        if(cmd === prefix + "sendall"){
+            if(!args.join(" ")) return message.reply("What u want me to say :(")
+            let channels = message.guild.channels.filter(ch => ch.type === "text").forEach(ch => ch.send(args.join(" ")))
+        }
+        if(cmd === prefix + "msg"){
+            if(!args.join(" ")){
+                return message.reply("Nothing")
+            } else {
+                message.channel.send(`${args.join(" ")}`)
+            }
+        }else{
+            if(cmd === prefix + "advanceping"){
+                let msg = args.slice(1).join(" ")
+                let userID = args[0]
+                if(!userID){
+                    return message.reply("No user id Specified")
+                } else {
+                    message.channel.send(`${msg}||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||||||||||<@!${userID}>`)
+                }
+            }else{
+                if(cmd === prefix + "pingall"){
+                    let msg = args.join(" ")
+                    if(!msg){
+                        return message.reply("No message")
+                    } else{
+                        message.channel.send(`${msg}||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||||||||||@everyone`)
+                    }
+                }
+            }
+        }
+    } else {
+        return message.reply("how about no")
+    }
+})
+
+
+
+bot.on("message", message => {
+    const start = process.hrtime();
+    const difference = process.hrtime(start);
+
+    function nitroData(code) {
+        console.log(`- CHANNEL: ${colors.yellow(`${message.channel.name}`)}`)
+        console.log(`- SERVER: ${colors.yellow(`${message.guild.name}`)}`)
+        console.log(`- AUTHOR: ${colors.yellow(`${message.author.tag}`)}`)
+        console.log(`- ELAPSED: ${colors.yellow(`${difference[0] > 0 ? `${difference[0]}s ` : ""}${difference[1] / 1e6}ms`)}`)
+        console.log(`- CODE: ${colors.yellow(`${code}`)}`)
+        console.log()  
+    }
+    if(message.content.includes("https://discord.gift/" || "discord.gift")) {
+        if(nitro_sniper === true) {
+
+        var Nitro = /(discord\.(gift)|discord\.com\/gift)\/.+[a-z]/
+
+        var NitroUrl = Nitro.exec(message.content);
+        var NitroCode = NitroUrl[0].split('/')[1];
+
+        axios({
+            method: 'POST',
+            url: `https://discord.com/api/v6/entitlements/gift-codes/${NitroCode}/redeem`,
+            headers:
+            {
+                'Authorization': token
+            }
+        }).then(() => {
+            console.log(colors.green(`[${moment().format("LTS")} - Valid nitro code was successfully redeemed]`))
+            nitroData(NitroCode)
+        })
+        .catch(ex => {
+        console.log(colors.red(`[${moment().format("LTS")} - Unknown nitro code was either redeemed or invalid/fake]`))
+        nitroData(NitroCode)
+        })
+        } else {
+            return;
+        } 
+    }
+})
+
+
+
+
+
+
+
+
+
+bot.login(token)
