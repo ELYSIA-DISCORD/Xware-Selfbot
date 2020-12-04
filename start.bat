b3a941354ce0cff431dd95b03e88e2af55651f01
DaGamingMan2018 <58614587+DaGamingMan2018@users.noreply.github.com>
2020-12-04 19:47:26 +1300

    Obfuscated & Updated

--- start.bat
+++ start.bat
@@ -1,2 +1,13 @@
+@echo off
+title Xware running
+echo Do you agree to everything said on the Xware github repository? yes or no
+set /p Agree=
+if %Agree%== yes goto Yes
+if %Agree%== no goto Exit
+:Exit
+Exit
+:Yes
+cls
 node index.js
 PAUSE
+goto Yes
